dd3a087844fef4353695e03421b8577d8a4eab00
Arseniy Shkunkov <shkunkov.a@yandex.ru>
2024-05-12 19:54:39 +0200

    [All]: Removed automatic docker-compose command from setup_aws script

--- setup_aws.sh
+++ setup_aws.sh
@@ -4,5 +4,3 @@ sudo systemctl start httpd
 
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-docker-compose up
